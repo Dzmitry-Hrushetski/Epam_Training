f2205d67c546bf5072d37e43260b8ddd7f8453bb
Dzmitry-Hrushetski <dzmitry.hrushetski@gmail.com>
2015-05-29 14:23:20 +0200

    Create project

---
+++ EpamTraining_Task_2/src/com/epam/text/bean/TypeText.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.epam.text.bean;
+
+/**
+ * @author Dzmitry Hrushetski 29 мая 2015 г.
+ * 
+ */
+public enum TypeText {
+
+}
---
+++ EpamTraining_Task_2/src/com/epam/text/runner/Runner.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.epam.text.runner;
+
+/**
+ * @author Dzmitry Hrushetski 29 мая 2015 г.
+ * 
+ */
+public class Runner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
