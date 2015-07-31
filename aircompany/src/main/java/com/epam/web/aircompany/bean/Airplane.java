47a62fed00535a2cefa01310e2d52005a589fc4b
Dzmitry-Hrushetski <dzmitry.hrushetski@gmail.com>
2015-07-31 13:12:31 +0200

    Save

--- aircompany/src/main/java/com/epam/web/aircompany/bean/Airplane.java
+++ aircompany/src/main/java/com/epam/web/aircompany/bean/Airplane.java
@@ -14,10 +14,15 @@ public class Airplane extends AirplaneType {
 	 */
 	private static final long serialVersionUID = 5802957980716076898L;
 	private String boardNumber;
-	
+	/**
+	 * @return the boardNumber
+	 */
 	public String getBoardNumber() {
 		return boardNumber;
 	}
+	/**
+	 * @param boardNumber the boardNumber to set
+	 */
 	public void setBoardNumber(String boardNumber) {
 		this.boardNumber = boardNumber;
 	}
